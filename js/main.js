282f9a5e2d74525de2e6451fac2cacd6de9cb3f1
antsupov.official@gmail.com <antsupov.official@gmail.com>
2019-07-06 18:55:18 +0700

    test2

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let lb = document.querySelector('#loginButton'),
+rb = document.querySelector('#registerButton'),
+lf = document.querySelector('.logForm'),
+rf = document.querySelector('.regForm');
+
+
+lb.addEventListener('click',()=>{
+    
+    lf.classList.add('vis');
+    
+
+});
+rb.addEventListener('click',()=>{
+    
+    rf.classList.add('vis');
+    
+
+});
